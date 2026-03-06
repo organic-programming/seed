a9664052b9094e97cb5a556e15fd4a3dc8c090f3
bpds <benoit@compilons.com>
2026-03-06 19:25:07 +0100

    Update go-dart-holons submodule: Flutter project renamed to gudule

---
+++ examples/cpp-hello-world/build-codex-audit/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bpds/Documents/Entrepot/Git/Compilons/videosteno/organic-programming/examples/cpp-hello-world
+# Build directory: /Users/bpds/Documents/Entrepot/Git/Compilons/videosteno/organic-programming/examples/cpp-hello-world/build-codex-audit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hello_unit_tests "/Users/bpds/Documents/Entrepot/Git/Compilons/videosteno/organic-programming/examples/cpp-hello-world/build-codex-audit/test_hello")
+set_tests_properties(hello_unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/bpds/Documents/Entrepot/Git/Compilons/videosteno/organic-programming/examples/cpp-hello-world/CMakeLists.txt;12;add_test;/Users/bpds/Documents/Entrepot/Git/Compilons/videosteno/organic-programming/examples/cpp-hello-world/CMakeLists.txt;0;")
