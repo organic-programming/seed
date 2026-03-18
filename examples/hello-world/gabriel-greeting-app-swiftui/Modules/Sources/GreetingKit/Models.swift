e28a3ff02062ce68c8046a9b91e57d8c2fd9f3fe
bpds <benoit@compilons.com>
2026-03-18 17:51:59 +0100

    feat: Enable dynamic app versioning in Info.plist and remove `@retroactive` from `Identifiable` conformance.

--- examples/hello-world/gabriel-greeting-app-swiftui/Modules/Sources/GreetingKit/Models.swift
+++ examples/hello-world/gabriel-greeting-app-swiftui/Modules/Sources/GreetingKit/Models.swift
@@ -1,6 +1,6 @@
 import SwiftProtobuf
 
 /// Make the generated Language type usable in SwiftUI lists.
-extension Greeting_V1_Language: @retroactive Identifiable {
+extension Greeting_V1_Language: Identifiable {
     public var id: String { code }
 }
