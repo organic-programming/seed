9aa23fc53053eac2b03d0cb5184ef440e2da9202
bpds <benoit@compilons.com>
2026-03-23 21:10:02 +0100

    feat(kotlin-holons): rebuild greeting example

--- examples/hello-world/gabriel-greeting-kotlin/src/main/kotlin/org/organicprogramming/gabriel/greeting/kotlinholon/cmd/Main.kt
+++ examples/hello-world/gabriel-greeting-kotlin/src/main/kotlin/org/organicprogramming/gabriel/greeting/kotlinholon/cmd/Main.kt
@@ -1,9 +1,12 @@
 package org.organicprogramming.gabriel.greeting.kotlinholon.cmd
 
+import gen.DescribeGenerated
 import org.organicprogramming.gabriel.greeting.kotlinholon.api.Cli
+import org.organicprogramming.holons.Describe
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    Describe.useStaticResponse(DescribeGenerated.StaticDescribeResponse())
     val exitCode = Cli.run(args)
     if (exitCode != 0) {
         exitProcess(exitCode)
