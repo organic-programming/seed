526b73533d1b05bbef55554ea2e029068534ec86
bpds <benoit@compilons.com>
2026-03-24 17:31:17 +0100

    chore: rebuild all gabriel examples with latest op

--- examples/hello-world/gabriel-greeting-rust/gen/describe_generated.rs
+++ examples/hello-world/gabriel-greeting-rust/gen/describe_generated.rs
@@ -19,7 +19,7 @@ pub fn static_describe_response() -> DescribeResponse {
                     composer: "B. ALTER".to_string(),
                     status: "draft".to_string(),
                     born: "2026-03-16".to_string(),
-                    version: "0.1.51".to_string(),
+                    version: "0.1.57".to_string(),
                     aliases: vec![],
                 }
             ),
